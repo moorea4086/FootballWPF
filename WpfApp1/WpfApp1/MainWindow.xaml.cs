2e35c129b5f6fe1b165c02f89ac9b32c8fa7b775
Alexander Moore <moorea4086@gmail.com>
2019-11-15 15:25:46 -0500

    Winform as WPF

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Football_Winforms;
 
 namespace WpfApp1
 {
@@ -22,5 +25,26 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Create the interop host control.
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            Matchup matchup = new Matchup("Eagles", "Patriots");
+            matchup.TopLevel = false;
+            matchup.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            matchup.Visible = true;
+            matchup.Dock = DockStyle.Fill;
+            host.Child = matchup;
+
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            this.grid1.Children.Add(host);
+        }
+        // https://docs.microsoft.com/en-us/dotnet/framework/wpf/advanced/walkthrough-hosting-a-windows-forms-control-in-wpf
+
     }
 }
